80cfc99634f69cb10930ed06f7efa41e071b87c0
BalatrillionDollars <127lmerino@frhsd.com>
2025-09-10 08:58:40 -0400

    last names (just trying push and pull and stuff)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, my name is Lucas!");
-        System.out.println("Howdy Neighbor, Nick here!");
-        System.out.println("Hello, my name is Hortencia.");
+        System.out.println("Hello, my name is Lucas Merino!");
+        System.out.println("Howdy Neighbor, Nick Coppernico here!");
+        System.out.println("Hello, my name is Hortencia Barrientos.");
     }
 }
